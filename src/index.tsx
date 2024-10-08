dd85382ff1203f67567e8464c25d9869eb1360f8
oluceps <secinia@outlook.com>
2024-10-08 08:36:34 +0800

    + jsdoc
    + bump

--- src/index.tsx
+++ src/index.tsx
@@ -15,8 +15,18 @@ export const LenisContext = createContext<LenisContextValue>();
 
 const [rootLenisContextStore, setRootLenisContextStore] = createStore<Partial<LenisContextValue>>();
 
-
-// Lenis hook in SolidJS
+/**
+ * Lenis hook in SolidJS
+ *
+ * This hook provides access to the Lenis instance and its methods. It allows you to register
+ * scroll callbacks and retrieve the Lenis instance. The callback is triggered during scroll
+ * events based on the Lenis smooth scrolling mechanism.
+ *
+ * @param {ScrollCallback} [callback] - The callback to be called on each scroll event.
+ * @param {number} [priority=0] - The priority of the callback in the execution order (lower numbers have higher priority).
+ *
+ * @returns {Lenis | undefined} - The Lenis instance or undefined if not available.
+ */
 export function useLenis(callback?: ScrollCallback, priority = 0) {
 	const fallbackContext: Partial<LenisContextValue> = {};
 	const localContext = useContext(LenisContext);
@@ -40,7 +50,17 @@ export function useLenis(callback?: ScrollCallback, priority = 0) {
 	return lenis;
 }
 
-// Lenis component in SolidJS
+/**
+ * Lenis component in SolidJS
+ *
+ * This component provides a smooth scrolling mechanism using the Lenis library. It manages the
+ * lifecycle of the Lenis instance, allows for custom scroll callbacks, and optionally integrates
+ * with the root scroll wrapper or content elements. It can be used for both root and nested scroll contexts.
+ *
+ * @param {LenisProps} props - The props for configuring the Lenis component.
+ *
+ * @returns {JSX.Element} - The JSX element that contains the scrollable content.
+ */
 export function SolidLenis(props: LenisProps) {
 	const [wrapperRef, setWrapperRef] = createSignal<HTMLDivElement | null>();
 	const [contentRef, setContentRef] = createSignal<HTMLDivElement | null>();
